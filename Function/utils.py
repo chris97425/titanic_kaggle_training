2ea33ac11d2500ea7396d3ee5f86ec7a3c228913
christopher de boisvilliers <christopher.deboisvilliers@gmail.com>
2022-07-22 10:00:54 +0400

    add who is with age and sex

---
+++ Function/utils.py
@@ -0,0 +1,22 @@
+import re
+
+def check_all_group(x):
+    if (x.Survived == 1).all():
+        return "Rich."
+    if (x.Survived == 0).all():
+        return "Poor."
+    else:
+        return "other"
+
+#Get the title of a name (mrs, ms etc) thanks to regex 👌🏼
+def get_title(user_name):
+    return  re.search(r', (.+?\.)',user_name).group(1)
+
+#Define a class by age and sex
+def class_of_people_by_age_sex(age, sex):
+    if age<=12:
+        return "Children"
+    elif age>=48 and sex=="female":
+        return "female_a_48"
+    else :
+        return "middle"
